9fab46010d05d697595313e35470d940c49b4d41
Ondrej Suchanek <osuchane@c3r3s3.42prague.com>
2022-10-29 18:07:34 +0200

    del commit

--- ex08/ex05main.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include "ex05/ft_show_tab.c"
-#include "ex05/ft_stock_str.h"
-#include "./ex04/ft_strs_to_tab.c"
-
-int	main(int argc, char **argv)
-{
-	ft_show_tab(ft_strs_to_tab(argc, argv));
-}
